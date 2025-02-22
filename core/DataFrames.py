45f927377f8a40875ca8d62fdf9f88dd1a122a70
armjay <45674942+jacobarmbrecht@users.noreply.github.com>
2025-02-21 17:11:32 -0700

    Added first step of validation process (calculating RMSE based on basal area)

--- core/DataFrames.py
+++ core/DataFrames.py
@@ -11,20 +11,91 @@ cond_selected_columns = ["CN", "PLT_CN", "SLOPE", "ASPECT", "BALIVE", "LIVE_CANO
 tree_selected_columns = ["CN", "PLT_CN", "DIA", "ACTUALHT","HT", "TPA_UNADJ"]
 plot_selected_columns = ["CN", "PLOT", "ELEV", "LAT", "LON"]
 
-
-
-
-def main():
-    print("main function")
-
-
-
-if __name__ == "__main__":
-    main()
-
 def create_polars_dataframe():
     print("Creating Polars DataFrame")
 
+    cond_df = pl.read_csv(os.path.join(data_dir, cond_fname), columns=cond_selected_columns)
+    #cond_df = cond_df.drop_nulls()
+    COND = cond_df.sort("PLT_CN")
+    tree_df = pl.read_csv(os.path.join(data_dir, tree_fname), columns=tree_selected_columns)
+    TREE = tree_df.sort("PLT_CN")
+    plot_df = pl.read_csv(os.path.join(data_dir, plot_fname), columns=plot_selected_columns)
+    #plot_df = plot_df.drop_nulls()
+    plot_df = plot_df.rename({"CN": "PLT_CN"})
+    PLOT = plot_df.sort("PLT_CN")
+
+    # grab diameters before grouping
+    # DIA = TREE.get_column("DIA")
+
+
+
+
+    # group trees by plot cn and join with our plot group
+    # In MT_CSV, ACTUALHT is generally empty or the same as HT
+    TREEGRP = pl.sql(
+        query='''
+            SELECT TREE.PLT_CN, 
+                    COUNT(TREE.CN) AS TREE_COUNT, 
+                    SUM(TREE.TPA_UNADJ), 
+                    SUM(POW(TREE.DIA,2)) AS DIA_SQR_SUM, 
+                    MAX(TREE.HT) AS MAX_HT, 
+                    AVG(TREE.HT) AS AVG_HT
+            FROM TREE
+            GROUP BY TREE.PLT_CN
+        '''
+    ).collect()
+
+
+    CONDGRP = pl.sql(
+        query='''
+        SELECT 
+        PLOT.PLT_CN,
+        PLOT.ELEV, 
+        PLOT.LAT, 
+        PLOT.LON, 
+        COND.SLOPE, 
+        COND.ASPECT, 
+        COND.LIVE_CANOPY_CVR_PCT, 
+        COND.BALIVE
+        FROM PLOT NATURAL LEFT JOIN COND
+        '''
+    ).collect()
+    print(CONDGRP)
+
+    #select CONDGRP PLT_CN for 40,000
+    #select TREEGRP PLT_CN for 12,000
+    FINAL = pl.sql(
+        query='''
+        SELECT DISTINCT
+            TREEGRP.PLT_CN,
+            TREEGRP.TREE_COUNT,
+            TREEGRP.TPA_UNADJ,
+            TREEGRP.MAX_HT,
+            TREEGRP.AVG_HT,
+            CONDGRP.ELEV,
+            CONDGRP.LAT,
+            CONDGRP.LON,
+            CONDGRP.SLOPE,
+            CONDGRP.ASPECT,
+            CONDGRP.LIVE_CANOPY_CVR_PCT,
+            CONDGRP.BALIVE
+            FROM CONDGRP NATURAL LEFT JOIN TREEGRP
+            
+        '''
+    ).collect()
+
+    print(FINAL)
+
+    FINAL = FINAL.unique(subset=["PLT_CN"])
+    FINAL = FINAL.drop_nulls()
+
+    print("Final DataFrame: ")
+    print(FINAL)
+    FINAL.write_csv(os.path.join(data_dir, "output.csv"), separator=",")
+    return FINAL
+
+def create_avg_polars_dataframe():
+    print("Creating Polars DataFrame with Average Values")
     cond_df = pl.read_csv(os.path.join(data_dir, cond_fname), columns=cond_selected_columns)
     # cond_df = cond_df.drop_nulls()
     COND = cond_df.sort("PLT_CN")
@@ -38,7 +109,10 @@ def create_polars_dataframe():
 
     PLOTGRP = pl.sql(
         query='''
-        SELECT PLOT.ELEV, PLOT.LAT, PLOT.LON, COALESCE(PLOT.CN, TREE.PLT_CN) AS PLT_CN,
+        SELECT PLOT.ELEV, 
+            PLOT.LAT, 
+            PLOT.LON, 
+            COALESCE(PLOT.CN, TREE.PLT_CN) AS PLT_CN,
         FROM PLOT NATURAL LEFT JOIN TREE
 
         '''
@@ -47,7 +121,14 @@ def create_polars_dataframe():
 
     CONDGRP = pl.sql(
         query='''
-        SELECT COND.SLOPE, COND.ASPECT, PLOT.ELEV, PLOT.LAT, PLOT.LON, COND.LIVE_CANOPY_CVR_PCT, COALESCE(COND.PLT_CN, PLOTGRP.PLT_CN) AS PLT_CN,
+        SELECT COND.BALIVE,
+            COND.SLOPE, 
+            COND.ASPECT, 
+            PLOT.ELEV, 
+            PLOT.LAT, 
+            PLOT.LON, 
+            COND.LIVE_CANOPY_CVR_PCT, 
+            COALESCE(COND.PLT_CN, PLOTGRP.PLT_CN) AS PLT_CN,
         FROM COND NATURAL LEFT JOIN PLOTGRP
         '''
     ).collect()
@@ -60,8 +141,8 @@ def create_polars_dataframe():
             SELECT TREE.PLT_CN, 
                     COUNT(TREE.CN) AS TREE_COUNT, 
                     AVG(COND.LIVE_CANOPY_CVR_PCT),
+                    AVG(COND.BALIVE),
                     SUM(TREE.TPA_UNADJ), 
-                    SUM(POW(TREE.DIA,2)) AS DIA_SQR_SUM, 
                     MAX(TREE.HT) AS MAX_HT, 
                     AVG(TREE.HT) AS AVG_HT, 
                     AVG(PLOT.ELEV),
@@ -75,13 +156,19 @@ def create_polars_dataframe():
         '''
     ).collect()
 
-    # calculate QMD from our data
-    DIAS = TREEGRP.get_column("DIA_SQR_SUM")
-    CNCOUNT = TREEGRP.get_column("TREE_COUNT")
-    QMD = np.sqrt(DIAS / CNCOUNT)
-    QMD = QMD.alias("QMD")
-    TREEGRP.insert_column(4, QMD)
 
     print("Final DataFrame: ")
     print(TREEGRP)
     return TREEGRP
+
+
+def main():
+    print("main function")
+    create_polars_dataframe()
+
+
+
+if __name__ == "__main__":
+    main()
+
+
--- core/RandomForest.py
+++ core/RandomForest.py
@@ -1,19 +1,23 @@
 import polars as pl
 import numpy as np
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
 from DataFrames import create_polars_dataframe
+from core.DataFrames import create_avg_polars_dataframe
 
 MAX_DEPTH = 6
+data_dir = "./data"
 
 def main():
-    FitRandomForest()
+    #fit_random_forest()
+    validate_datasets()
 
 
 
-def FitRandomForest():
+def fit_random_forest():
     plot_data = create_polars_dataframe()
 
     #features and target data
@@ -23,7 +27,7 @@ def FitRandomForest():
                    'TPA_UNADJ',
                    'MAX_HT',
                    'AVG_HT',
-                   'QMD',
+                   'BALIVE',
                    'ELEV',
                    'SLOPE',
                    'ASPECT',
@@ -32,25 +36,93 @@ def FitRandomForest():
                    ]]
     y = plot_data['PLT_CN']
 
-    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.5)
+    X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=.2)
+
+
 
     #initialize RandomForestClassifier
     rf_classifier = RandomForestClassifier(n_estimators=100)
 
     print("Fitting RandomForestClassifier to dataset")
     #fit classifier to data
-    rf_classifier.fit(X_train, y_train)
+    rf_classifier.fit(X_train,y_train)
 
     #make prediction
     y_pred = rf_classifier.predict(X_test)
+    y_pred = pl.from_numpy(y_pred, orient='col')
+    y_test = y_test.to_frame()
+    #X_test = X_test.to_frame()
 
-    #get accuracy and classification reports
-    accuracy = accuracy_score(y_test, y_pred)
-    classification_rep = classification_report(y_test, y_pred)
+    #save our data
+    save_data(os.path.join(data_dir, "y_pred.csv"), y_pred)
+    save_data(os.path.join(data_dir, "X_test.csv"), X_test)
+    save_data(os.path.join(data_dir, "y_test.csv"), y_test)
+    save_data(os.path.join(data_dir, "plot_data.csv"), plot_data)
 
-    #print
-    print(f"Accuracy: {accuracy:.2f}")
-    print("\nClassification Report:\n", classification_rep)
+    #validate our results
+    validate_results(y_pred, X_test,y_test,plot_data)
+
+def validate_datasets():
+    y_pred = pl.read_csv(os.path.join(data_dir, "y_pred.csv"))
+    y_test = pl.read_csv(os.path.join(data_dir, "y_test.csv"))
+    X_test = pl.read_csv(os.path.join(data_dir, "X_test.csv"))
+    plot_data = pl.read_csv(os.path.join(data_dir, "plot_data.csv"))
+    validate_results(y_pred, X_test, y_test, plot_data)
+
+def validate_results(y_pred, X_test, y_test, plot_data):
+    print("Validating results: ")
+
+    #organize our predictions into a data frame
+    #   then connect it to our X testing data
+    #   This forms our predictions of test data
+    y_pred = y_pred.rename({"column_0": "PLT_CN"})
+    pred = pl.concat([y_pred, X_test], how="horizontal")
+    pred = pred.sort("PLT_CN")
+    n = len(pred) #get our total number of plots
+    print(pred)
+
+    TEST = pl.sql(
+        query='''
+            SELECT 
+            pred.PLT_CN,
+            pred.BALIVE
+            FROM pred
+            '''
+    ).collect()
+    print(TEST)
+
+    #Now grab our original plot data based on predicted PLT_CN
+    PLT = pl.sql(
+        query='''
+            SELECT 
+            y_pred.PLT_CN,
+            plot_data.BALIVE
+            FROM y_pred NATURAL INNER JOIN plot_data
+            '''
+    ).collect()
+    PLT = PLT.sort("PLT_CN")
+    print(PLT)
+
+
+
+
+    SUB = TEST - PLT
+    print(SUB)
+
+    SUMSQ= pl.sql(
+        query='''
+            SELECT 
+            SUM(POW(SUB.BALIVE,2))
+            FROM SUB
+            '''
+    ).collect()
+
+    #final step of calculating RMSE
+    RMSE = np.sqrt(SUMSQ.item()/n)
+    print(f"RMSE: {RMSE}")
+
+def save_data(path, frame):
+    frame.write_csv(path, separator=",")
 
 
 if __name__ == "__main__":
